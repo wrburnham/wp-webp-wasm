7ce5893b5c16358abce3805fd5458dee03fc65ff
william <burnham.william@gmail.com>
2020-06-13 12:14:31 +0200

    update license information

--- main.php
+++ main.php
@@ -9,7 +9,7 @@ Description: Client side JPEG and PNG conversions to webp and upload.
 Version: 1.0
 Author: wrburnham
 Author URI: https://wrburnham.github.io
-License: MIT
+License: GPLv3
 Text Domain: webp-wasm
 */
 /*
@@ -19,8 +19,14 @@ images to webp in a web browser.
 See also: https://chromium.googlesource.com/webm/libwebp/
 
 The following is taken from https://www.webmproject.org/license/software/
+and included in this project as the webp.wasm file is a web assembly binary that
+leverages libwebp:
 
 Copyright (c) 2010, Google Inc. All rights reserved.
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+    Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    Neither the name of Google nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
